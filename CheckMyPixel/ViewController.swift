87b82867ccab57b84aa96945658b0854852fd90f
Stasik <scherkasov8@gmail.com>
2018-01-17 23:00:29 +0200

    all is done but without google adv

---
+++ CheckMyPixel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CheckMyPixel
+//
+//  Created by Stanislav Cherkasov on 17.01.18.
+//  Copyright © 2018 Stanislav Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+}
+
