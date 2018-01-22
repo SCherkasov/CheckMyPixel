e645b9b19abc97df51914417a0219b7acfd597f5
Stasik <scherkasov8@gmail.com>
2018-01-22 22:56:32 +0200

    integrated two banners google (AdMob)

--- CheckMyPixel/Controller/ViewController.swift
+++ CheckMyPixel/Controller/ViewController.swift
@@ -7,9 +7,34 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GADBannerViewDelegate {
 
+    @IBOutlet weak var bottomBanner: GADBannerView!
+    
+    @IBOutlet weak var topBanner: GADBannerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Request
+        let request = GADRequest()
+        request.testDevices = [kGADSimulatorID]
+        
+        //Setup bottomBanner
+        bottomBanner.adUnitID = "ca-app-pub-1533289263767155/2440927757"
+        bottomBanner.rootViewController = self
+        bottomBanner.delegate = self
+        bottomBanner.load(request)
+        
+        //Setup topBanner
+        topBanner.adUnitID = "ca-app-pub-1533289263767155/8177304315"
+        topBanner.rootViewController = self
+        topBanner.delegate = self
+        topBanner.load(request)
+    }
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
